46da6cf8e85a6b0bd47e3f2cfef38bb3fc1a069a
Nikita Fedorenko <nikita.fedorenko@digitalturbine.com>
2023-07-02 14:50:29 +0300

    update protobuf to 30007

--- GPBProtocolBuffers.m
+++ GPBProtocolBuffers.m
@@ -28,14 +28,38 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-// This header is meant to only be used by the generated source, it should not
-// be included in code using protocol buffers.
+// If you want to build protocol buffers in your own project without adding the
+// project dependency, you can just add this file.
 
-// clang-format off
-#import "GPBBootstrap.h"
-// clang-format on
+// This warning seems to treat code differently when it is #imported than when
+// it is inline in the file.  GPBDictionary.m compiles cleanly in other targets,
+// but when #imported here it triggers a bunch of warnings that don't make
+// much sense, and don't trigger when compiled directly.  So we shut off the
+// warnings here.
+#pragma clang diagnostic ignored "-Wnullability-completeness"
 
-#import "GPBDescriptor_PackagePrivate.h"
-#import "GPBMessage.h"
-#import "GPBRootObject_PackagePrivate.h"
-#import "GPBUtilities_PackagePrivate.h"
+#import "GPBArray.m"
+#import "GPBCodedInputStream.m"
+#import "GPBCodedOutputStream.m"
+#import "GPBDescriptor.m"
+#import "GPBDictionary.m"
+#import "GPBExtensionInternals.m"
+#import "GPBExtensionRegistry.m"
+#import "GPBMessage.m"
+#import "GPBRootObject.m"
+#import "GPBUnknownField.m"
+#import "GPBUnknownFieldSet.m"
+#import "GPBUtilities.m"
+#import "GPBWellKnownTypes.m"
+#import "GPBWireFormat.m"
+
+#import "GPBAny.pbobjc.m"
+#import "GPBApi.pbobjc.m"
+#import "GPBDuration.pbobjc.m"
+#import "GPBEmpty.pbobjc.m"
+#import "GPBFieldMask.pbobjc.m"
+#import "GPBSourceContext.pbobjc.m"
+#import "GPBStruct.pbobjc.m"
+#import "GPBTimestamp.pbobjc.m"
+#import "GPBType.pbobjc.m"
+#import "GPBWrappers.pbobjc.m"
